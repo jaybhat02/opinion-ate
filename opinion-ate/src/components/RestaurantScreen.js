9cde2d511ac9376ac2dd278af6b38592fb051ff1
jaybhat02 <jay.bhat02@gmail.com>
2023-03-08 19:58:44 -0500

    Load restaurants upon first rendering RestaurantList

--- opinion-ate/src/components/RestaurantScreen.js
+++ opinion-ate/src/components/RestaurantScreen.js
@@ -1,6 +1,8 @@
 import RestaurantList from './RestaurantList';
 export default function RestaurantScreen() {
     return (
-        <div> <h1>Restaurants</h1> <RestaurantList />
+        <div>
+            <h1>Restaurants</h1>
+            <RestaurantList />
         </div>);
 }
